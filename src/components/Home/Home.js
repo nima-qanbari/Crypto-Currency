4bbdeec44f22426dd8110703d820e6d9e492bd30
nima <nimaqanbari1999@gmail.com>
2022-02-21 09:20:44 -0800

    add Loader

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,36 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 //API
-import { getCoins } from '../../Services/API'
+import { getCoins } from "../../Services/API";
 
 //components
-import Coin from '../Coin/Coin'
+import Coin from "../Coin/Coin";
+import Loader from "../Loader/Loader";
 
 const Home = () => {
-    const [ coins, setCoins ] = useState([])
+  const [coins, setCoins] = useState([]);
 
-    useEffect(() => {
-        const fetchApi = async () => {
-            setCoins(await getCoins())
-        }
-        return fetchApi()
-    }, [])
+  useEffect(() => {
+    const fetchApi = async () => {
+      setCoins(await getCoins());
+    };
+    return fetchApi();
+  }, []);
 
   return (
     <div>
-        {
-            coins.map(item => <Coin
-                key={item.id}
-                image={item.image}
-                name={item.name}
-                symbol={item.symbol}
-                price={item.current_price}
-                marketCap={item.market_cap}
-                priceChange={item.price_change_percentage_24h}
-                />)
-        }
+      {coins.length ? (
+        coins.map((item) => (
+          <Coin
+            key={item.id}
+            image={item.image}
+            name={item.name}
+            symbol={item.symbol}
+            price={item.current_price}
+            marketCap={item.market_cap}
+            priceChange={item.price_change_percentage_24h}
+          />
+        ))
+      ) : (
+        <Loader />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
